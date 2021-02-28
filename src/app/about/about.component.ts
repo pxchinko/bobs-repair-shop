da51e4e376b5720a76d28111945a5a83a67b17ee
Sarah Massie <smassie@my365.bellevue.edu>
2021-02-27 20:56:31 -0800

    master

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,3 +1,10 @@
+/**
+ * Title: about.component.ts
+ * Author: Sarah Massie
+ * Date: 25 February 2021
+ * Description: about component
+ */
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
